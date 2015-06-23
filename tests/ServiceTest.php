19367f3b02a79e6de410444b177b1745eb1255e6
Richard Wossal <richard@r-wos.org>
2015-06-23 19:47:43 +0200

    test setup

---
+++ tests/ServiceTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (c) 2015 Imagine Easy Solutions LLC
+ * MIT licensed, see LICENSE file for details.
+ */
+namespace EasyBib\Test\Silex\Salesforce;
+
+class ServiceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFoo()
+    {
+        $this->assertTrue(true);
+        $this->assertTrue(false);
+    }
+}
